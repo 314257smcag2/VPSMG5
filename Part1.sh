533d2b5ddb002faca28335f0719e8de995e1f9b2
314257smcag2 <98874964+314257smcag2@users.noreply.github.com>
2023-01-11 19:38:14 +0100

    Update Part1.sh

--- Part1.sh
+++ Part1.sh
@@ -2,6 +2,5 @@ rm -rf Proot
 mkdir Proot 
 cd Proot
 wget https://raw.githubusercontent.com/314257smcag2/VPSMG5/main/Part2.sh
-curl -skLo gdown https://raw.githubusercontent.com/kmille36/GoogleDriveCurl/main/gdown && chmod +x gdown
-bash gdown focal.tar 1rncRR9Gimh8UgWkAo27Q6X8UW5iOXTnL && tar -xf  focal.tar && rm -rf focal.tar
+wget https://uk.lxd.images.canonical.com/images/ubuntu/focal/amd64/cloud/20230107_07:42/rootfs.tar.xz && tar -xf rootfs.tar.xz && rm -rf rootfs.tar.xz
 curl -LO https://proot.gitlab.io/proot/bin/proot && chmod +x proot && ./proot -S . bash -c "chmod +x Part2.sh && sudo su -c "./Part2.sh""
